ea9bb12c789536a8727b425ee0f4c2cf4077ad6d
sup3r0001 <forsterandrew15@gmail.com>
2024-09-18 15:43:54 -0700

    Added InitBoard and ShuffleBoard methods

--- MinesweeperLibrary/Board.cs
+++ MinesweeperLibrary/Board.cs
@@ -36,25 +36,72 @@ namespace MinesweeperLibrary
             EndTime = endTime;
         }
 
+        // Sets board size and bomb count based on difficulty,
         public void SetDifficulty(int difficulty)
         {
             switch (difficulty)
             {
                 case 1:
                     BoardSize = 9;
-                    BombCount = BoardSize * 2;
+                    BombCount = BoardSize * 2; // 18
                     break;
                 case 2:
                     BoardSize = 16;
-                    BombCount = BoardSize * 2;
+                    BombCount = BoardSize * 2; // 32
                     break;
                 case 3:
                     BoardSize = 24;
-                    BombCount = BoardSize * 2;
+                    BombCount = BoardSize * 2; // 48
                     break;
             }
         }
 
+        // Initializes the board with cells & bombs, then shuffles the board to randomize bomb placement
+        // Will then display the board
+        public String InitBoard()
+        {
+
+            Cells = new Cell[BoardSize, BoardSize];
+
+            int I = 0;
+            for (int row = 0; row < BoardSize; row++)
+            {
+                for (int col = 0; col < BoardSize; col++)
+                {
+                    I++;
+                    Cells[row, col] = new Cell(false, (BombCount <= I), false, 0, (row, col));
+                }
+            }
+
+            ShuffleBoard();
+
+            return "Board Initialized"; // Change to display board
+
+        }
+
+        public void ShuffleBoard()
+        {
+            ShuffleBoard(5);
+        }
+        public void ShuffleBoard(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                Random random = new Random();
+                for (int row = 0; row < BoardSize; row++)
+                {
+                    for (int col = 0; col < BoardSize; col++)
+                    {
+                        int newRow = random.Next(BoardSize);
+                        int newCol = random.Next(BoardSize);
+
+                        Cell temp = Cells[row, col];
+                        Cells[row, col] = Cells[newRow, newCol];
+                        Cells[newRow, newCol] = temp;
+                    }
+                }
+            }
+        }
 
     }
 }
