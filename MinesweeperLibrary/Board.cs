cf8517ffbd152dac8ccb9da83891a9a77050f414
sup3r0001 <forsterandrew15@gmail.com>
2024-09-18 15:20:34 -0700

    Added Constructors

--- MinesweeperLibrary/Board.cs
+++ MinesweeperLibrary/Board.cs
@@ -8,5 +8,53 @@ namespace MinesweeperLibrary
 {
     internal class Board
     {
+
+        public int BoardSize { get; set; }
+        public int BombCount { get; set; }
+        public int Difficulty { get; set; }
+        public Cell[,] Cells { get; set; }
+        public string StartTime { get; set; }
+        public string EndTime { get; set; }
+
+        public Board()
+        {
+            BoardSize = 0;
+            BombCount = 0;
+            Difficulty = 0;
+            Cells = new Cell[0, 0];
+            StartTime = "";
+            EndTime = "";
+        }
+
+        public Board(int boardSize, int bombCount, int difficulty, Cell[,] cells, string startTime, string endTime)
+        {
+            BoardSize = boardSize;
+            BombCount = bombCount;
+            Difficulty = difficulty;
+            Cells = cells;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
+        public void SetDifficulty(int difficulty)
+        {
+            switch (difficulty)
+            {
+                case 1:
+                    BoardSize = 9;
+                    BombCount = BoardSize * 2;
+                    break;
+                case 2:
+                    BoardSize = 16;
+                    BombCount = BoardSize * 2;
+                    break;
+                case 3:
+                    BoardSize = 24;
+                    BombCount = BoardSize * 2;
+                    break;
+            }
+        }
+
+
     }
 }
--- MinesweeperLibrary/Cell.cs
+++ MinesweeperLibrary/Cell.cs
@@ -8,11 +8,30 @@ namespace MinesweeperLibrary
 {
     internal class Cell
     {
+
         public bool IsMine { get; set; }
         public bool IsFlagged { get; set; }
         public bool IsRevealed { get; set; }
         public int AdjacentMines { get; set; }
+        public (int row, int col) Position { get; set; }
 
+        public Cell()
+        {
+            IsMine = false;
+            IsFlagged = false;
+            IsRevealed = false;
+            AdjacentMines = 0;
+            Position = (0, 0);
+        }
+
+        public Cell(bool isMine, bool isFlagged, bool isRevealed, int adjacentMines, (int row, int col) position)
+        {
+            IsMine = isMine;
+            IsFlagged = isFlagged;
+            IsRevealed = isRevealed;
+            AdjacentMines = adjacentMines;
+            Position = position;
+        }
 
     }
 }
