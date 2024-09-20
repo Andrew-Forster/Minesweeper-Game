75ce6f330377232f6578548a11124034ac58eacc
filib <filib@MSI>
2024-09-19 18:21:39 -0700

    Added while statment

--- MinesweeperConsoleApp/Program.cs
+++ MinesweeperConsoleApp/Program.cs
@@ -2,3 +2,18 @@
 
 Board board = new Board(1);
 Console.WriteLine(board.DisplayBoard());
+ 
+int choice = -1;
+
+DisplayMessage(" Choose Difficulty ");
+
+
+while (choice ! = 0)
+{
+  switch(choice)
+    {
+        case 1:
+
+           
+    }
+}
