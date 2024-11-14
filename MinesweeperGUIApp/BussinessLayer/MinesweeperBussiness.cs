6b6a0c3539f0faf698bbb2786b8bccce5e0c504c
filib <filib@MSI>
2024-11-14 14:06:58 -0700

    Made bussiness layer folder with a class for the bussiness layer named MinesweeperBussiness. And added the logic for the name entry in there.

---
+++ MinesweeperGUIApp/BussinessLayer/MinesweeperBussiness.cs
@@ -0,0 +1,32 @@
+﻿using MinesweeperGUIApp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MinesweeperLibrary.BussinessLayer
+{
+    internal class MinesweeperBussiness
+    {
+        
+        public string GetPlayersName()
+        {
+            // Dealre and Initalize 
+            string playerName = "";
+
+            // Show the Name entry form 
+            using (FrmNameEntry frmNameEntry = new FrmNameEntry())
+            {
+                // Chekc to make sure we recived input from the form 
+                if (frmNameEntry.ShowDialog() == DialogResult.OK)
+                {
+                    // Get the players name from frmNameEntry
+                    playerName = frmNameEntry.playerName;
+                }
+            }
+            return playerName;
+        }//End of Get Players Name
+    }
+}
